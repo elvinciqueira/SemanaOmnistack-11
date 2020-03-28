69da9735b01df9faf0d6030cd8a85420a69c8e18
elvinciqueira <elvinciqueira@gmail.com>
2020-03-28 15:56:24 -0300

    feat: adicionando validação

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import { celebrate, Segments, Joi } from 'celebrate';
 
 import OngController from './controllers/OngController';
 import IncidentController from './controllers/IncidentController';
@@ -10,12 +11,74 @@ const routes = new Router();
 routes.post('/sessions', SessionController.create);
 
 routes.get('/ongs', OngController.index);
-routes.post('/ongs', OngController.create);
 
-routes.get('/incidents', IncidentController.index);
-routes.post('/incidents', IncidentController.create);
-routes.delete('/incidents/:id', IncidentController.delete);
+routes.post(
+  '/ongs',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys({
+      name: Joi.string().required(),
+      email: Joi.string()
+        .required()
+        .email(),
+      whatsapp: Joi.number()
+        .min(10)
+        .max(11),
+      city: Joi.string().required(),
+      uf: Joi.string()
+        .required()
+        .length(2),
+    }),
+  }),
+  OngController.create
+);
 
-routes.get('/profile', ProfileController.index);
+routes.get(
+  '/incidents',
+  celebrate({
+    [Segments.QUERY]: Joi.object().keys({
+      page: Joi.number(),
+    }),
+  }),
+  IncidentController.index
+);
+
+routes.post(
+  '/incidents',
+  celebrate({
+    [Segments.BODY]: Joi.object().keys(
+      {
+        title: Joi.string().required(),
+        description: Joi.string().required(),
+        value: Joi.number().required(),
+      },
+      {
+        [Segments.HEADERS]: Joi.object({
+          authorization: Joi.string().required(),
+        }).unknown(),
+      }
+    ),
+  }),
+  IncidentController.create
+);
+
+routes.delete(
+  '/incidents/:id',
+  celebrate({
+    [Segments.PARAMS]: Joi.object().keys({
+      id: Joi.number().required(),
+    }),
+  }),
+  IncidentController.delete
+);
+
+routes.get(
+  '/profile',
+  celebrate({
+    [Segments.HEADERS]: Joi.object({
+      authorization: Joi.string().required(),
+    }).unknown(),
+  }),
+  ProfileController.index
+);
 
 export default routes;
